f9c561ea09b72c30e8438dc703cdab78de0891c8
HK7470 <ASUS@LAPTOP-VIEOFEFH>
2026-01-02 23:29:11 +0800

    v0.1.7e-fix1

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -23,6 +23,7 @@ import mindustry.world.blocks.environment.OreBlock;
 import mindustry.world.blocks.power.BeamNode;
 import mindustry.world.blocks.production.Drill;
 import mindustry.world.blocks.production.GenericCrafter;
+import mindustry.world.draw.DrawDefault;
 import mindustry.world.draw.DrawLiquidTile;
 import mindustry.world.draw.DrawMulti;
 import mindustry.world.draw.DrawTurret;
@@ -99,6 +100,7 @@ public class ExampleJavaMod extends Mod{
             consumePower(1f);
             outputLiquids=new LiquidStack[]{new LiquidStack(Liquids.hydrogen, 0.2f)};
             drawer=new DrawMulti(){{
+                new DrawDefault();
                 new DrawLiquidTile(Liquids.water);
             }};
         }};
